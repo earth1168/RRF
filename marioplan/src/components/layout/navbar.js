bf6e4f1946daa20bd6ec9406a5850e9767e7e06a
Earth <pimanus1234@gmail.com>
2019-04-26 18:02:23 +0700

    add dashboard

--- marioplan/src/components/layout/navbar.js
+++ marioplan/src/components/layout/navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import SignedInLink from './SignedInLink'
+import SignedOutLink from './SignedOutLink'
 const Navbar = () => {
     return(
         <nav className="nav-wrapper grey darken-3">
         <div className="container">
             <Link to ='/' className="brand-logo">Marioplan</Link>
             <SignedInLink />
+            <SignedOutLink />
         </div>
         </nav>
     )
